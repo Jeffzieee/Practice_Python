2312f0701e45ba7172a2385549361c2fa6d58fca
Jephin John <jephinjohn85@gmail.com>
2022-07-06 23:57:05 +0530

    Dice Roller GUI Game Completed

--- GUI Dice Roller Game/GUI Dice Roller Game.py
+++ GUI Dice Roller Game/GUI Dice Roller Game.py
@@ -1,51 +1,70 @@
-#Python Tkinter GUI Game
+# Python Tkinter GUI Dice Roller Guessing Game
 
-#importing dependancies
+# importing dependancies
 from tkinter import *
 from random import randint
+from PIL import ImageTk, Image
 
-#declaring and initializing global variables
-a=0
-count = 3
-
-#object creation for tkinter
+# object creation for tkinter
 game = Tk()
 game.geometry("570x600+100+20")
 game.resizable(False, False)
-game.title("Jephin's Python Tkinter GUI Guessing Game")
+game.title("Python Tkinter GUI Dice Roller Guessing Game")
 
-#guessButton Function Definition
-def btnGuessAction():
-    global count
-    global a
-    if(count==3):
-        a = randint(0, 11)
 
-    if count<=3 and count>=1:
-        x = int(guessText.get("1.0", "end-1c"))
-        if x == a:
-            result = Label(game, height=1, width=40, font=("arial", 14),text="Guessed Correctly with " + str(count) + " tries remaining.")
-            result.place(x=150, y=150)
-            count=3
-        elif x < a:
-            count -= 1
-            result = Label(game, height=1, width=40, font=("arial", 14),text="Guessed Less." + str(count) + " tries remaining.")
-            result.place(x=150, y=150)
-        elif x > a:
-            count-=1
-            result = Label(game, height=1, width=40, font=("arial", 14),text="Guessed greater. " + str(count) + " tries remaining.")
-            result.place(x=150, y=150)
+# function to load dieface image
+def load_dieface_img(face):
+    if (face == 1):
+        photo_one = Image.open("E:\Documents\Projects\PyCharm\Practice Python\GUI Dice Roller Game\Dice - png\One.png")
+        return photo_one
+    elif (face == 2):
+        photo_two = Image.open("E:\Documents\Projects\PyCharm\Practice Python\GUI Dice Roller Game\Dice - png\Two.png")
+        return photo_two
+    elif (face == 3):
+        photo_three = Image.open(
+            "E:\Documents\Projects\PyCharm\Practice Python\GUI Dice Roller Game\Dice - png\Three.png")
+        return photo_three
+    elif (face == 4):
+        photo_four = Image.open(
+            "E:\Documents\Projects\PyCharm\Practice Python\GUI Dice Roller Game\Dice - png\Four.png")
+        return photo_four
+    elif (face == 5):
+        photo_five = Image.open(
+            "E:\Documents\Projects\PyCharm\Practice Python\GUI Dice Roller Game\Dice - png\Five.png")
+        return photo_five
+    elif (face == 6):
+        photo_six = Image.open("E:\Documents\Projects\PyCharm\Practice Python\GUI Dice Roller Game\Dice - png\Six.png")
+        return photo_six
+    return 0
 
-    if (count == 0):
-        count = 3
-        result = Label(game, height=1, width=40, font=("arial", 14), text="You are out tries, Try Again! with new Number")
-        result.place(x=150, y=150)
 
-#Declaring GUI Components
-guessLabel = Label(game, height=1, width=30, font=("arial", 14), text="Guess a number from 0 to 10: ").place(x=0, y=25)
+# guess button action definition
+def btn_guess_action():
+    a = randint(1, 7)
+
+    photo = load_dieface_img(a)
+    photo_resize = photo.resize((300, 205), Image.ANTIALIAS)
+    new_photo = ImageTk.PhotoImage(photo_resize)
+    die_label = Label(image=new_photo)
+    die_label.image = new_photo
+    die_label.place(x=150, y=150)
+
+    x = int(guessText.get("1.0", "end-1c"))
+
+    if x == a:
+        result = Label(game, height=1, width=40, font=("arial", 14), text="Guessed Correctly.")
+        result.place(x=150, y=500)
+    else:
+        result = Label(game, height=1, width=40, font=("arial", 14), text="Guessed Wrong.")
+        result.place(x=150, y=500)
+
+
+# Declaring GUI Components
+guessLabel = Label(game, height=1, width=30, font=("arial", 14), text="Guess a die face ").place(x=0, y=25)
 resultLabel = Label(game, height=1, width=10, font=("arial", 14), text="RESULT: ").place(x=0, y=150)
-guessText = Text(game, height=1, width=15,font=("arial",14))
-guessText.place(x=350,y=25)
-guessBtn = Button(game, command=btnGuessAction,height=1, width=15, font=("arial", 14), text="GUESS!").place(x=200, y=80)
+guessText = Text(game, height=1, width=15, font=("arial", 14))
+guessText.place(x=350, y=25)
+guessBtn = Button(game, command=btn_guess_action, height=1, width=15, font=("arial", 14), text="ROLL!").place(x=200, y=80)
 
+#running game
 game.mainloop()
